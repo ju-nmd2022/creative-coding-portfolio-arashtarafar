04762dfd055e7e9d83d372b038ca1f1b80ffeec4
Arash <arashtarafar@users.noreply.github.com>
2024-09-08 01:18:40 +0200

    spiiiin

--- experiments/equalizer.js
+++ experiments/equalizer.js
@@ -4,19 +4,19 @@ let scale = 55;
 let resolution = 0.002;
 let numPoints = 30;
 let time = 0.01;
-let radius = windowHeight > windowWidth? 300 + windowHeight/2: 300 + windowWidth/2;
-let numSteps = 200;
+let radius = 200;
+let numSteps = 100;
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
     frameRate(60);
     // noLoop(); 
-    stroke(10, 0, 60, 100);
-    strokeWeight(5);
+    stroke(110, 10, 160, 100);
+    strokeWeight(3);
     noFill();
 }
 function draw() {
-  background(220);
+  background(20, 5, 30);
   for(let r = 0; r < radius; r += radius / numSteps){
     if(Math.random() < 0.3){
         continue;
