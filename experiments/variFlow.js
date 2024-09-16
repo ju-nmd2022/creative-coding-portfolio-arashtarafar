d7a5291b687e3542e62142b85ebde44e54a88f2b
Arash <arashtarafar@users.noreply.github.com>
2024-09-16 22:08:58 +0200

    Flow field got scarier

---
+++ experiments/variFlow.js
@@ -0,0 +1,73 @@
+let points;
+let directionChangeRate;
+let density;
+let gap;
+let time = 0.01;
+let timeStep = 0.02;
+
+let r1, r2, g1, g2, b1, b2; 
+
+function setup(){
+    points = [];
+    density = 55;
+    gap = width / density;
+
+    createCanvas(windowWidth, windowHeight);
+    frameRate(60);
+    // noLoop();
+    background(0);
+    angleMode(DEGREES); 
+    noiseDetail(1);    
+
+    for(let x = 0; x < width; x += gap){
+        for(let y = 0; y < height; y += gap){
+            points.push(createVector(x + random(-10, 10), y + random(-10, 10)));
+        }
+    }
+
+    shuffle(points, true);
+
+    r1 = random(255);
+    r2 = random(255);
+    g1 = random(255);
+    g2 = random(255);
+    b1 = random(255);
+    b2 = random(255);
+
+    directionChangeRate = random(0.001, 0.009);
+}
+function draw(){
+    noStroke();
+
+    let max;
+
+    if(frameCount <= points.length){
+        max = frameCount;
+    } else{
+        max = points.length; 
+    }
+
+    for(let iterator = 0; iterator < max; iterator++){
+        let thickness;
+        push();
+        let r = map(points[iterator].x, 0, width, r1, r2);
+        let g = map(points[iterator].y, 0, height, g1, g2); 
+        let b = map(points[iterator].x, 0, width, b1, b2);
+        let shutOffValue = width > height ? width : height;
+        
+        thickness = map(noise(time), 0, 1, 0.1, 3.2);
+        let alpha = map(dist(width/2, height/2, points[iterator].x, points[iterator].y), 0, shutOffValue / 2, 255, 0);
+        
+        fill(r, g, b, alpha);
+
+        ellipse(points[iterator].x, points[iterator].y, thickness);
+        pop();
+        let angle = map(noise(points[iterator].x * directionChangeRate, points[iterator].y * directionChangeRate), 0, 1, 0, 720);
+        points[iterator].add(createVector(cos(angle), sin(angle)));
+    }
+    time += timeStep;
+}
+
+function doubleClicked(){
+    saveCanvas("flowField", "png");  
+} 
