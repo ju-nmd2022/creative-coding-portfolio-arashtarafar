c3a03ac1ff3a45b9d459a0232cd7d236632623d6
Arash <arashtarafar@users.noreply.github.com>
2024-09-16 20:11:17 +0200

    All game no wife

--- experiments/cgol.js
+++ experiments/cgol.js
@@ -26,6 +26,33 @@ function setup(){
 }
 function draw(){
     background(0);
+
+    let next = make2DArray(columns, rows);
+
+    // compute next generation
+    for(let i = 0; i < columns; i++){
+        for(j = 0; j < rows; j++){
+            if(i === 0 || i === columns - 1 || j === 0 || j === rows - 1){
+                next[i][j] = grid[i][j];
+            }
+            else{
+                let sum = countNeighbors(grid, i, j);
+                let state = grid[i][j];
+                if(state === 1 && sum === 3){
+                    next[i][j] = 1;
+                } else if(state === 1 && (sum < 2 || sum > 3)){
+                    next[i][j] = 0;
+                } else if(state === 0 && sum === 3){
+                    next[i][j] = 1;
+                } else{
+                    next[i][j] = grid[i][j];
+                }
+            }
+        }
+    }
+
+    grid = next;
+
     for(let i = 0; i < columns; i++){
         for(j = 0; j < rows; j++){
             push();
@@ -39,4 +66,15 @@ function draw(){
             pop();
         }
     }
+}
+
+function countNeighbors(grid, x, y){
+    let sum = 0;
+    for(let i = -1; i < 2; i++){
+        for(let j = -1; j < 2; j++){
+            sum += grid[x + i][y + j];
+        }
+    }
+    sum -= grid[x][y];
+    return sum;
 }
