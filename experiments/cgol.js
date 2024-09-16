729bd313f78d0adffcf0ad43c724ac81d55dad69
Arash <arashtarafar@users.noreply.github.com>
2024-09-16 19:05:00 +0200

    Initialized matrix

--- experiments/cgol.js
+++ experiments/cgol.js
@@ -1,11 +1,22 @@
 let grid;
+let columns = 10;
+let rows = 0;
 
-function make2DArray(){
-    
+function make2DArray(cols, rows){
+    let array = new Array(cols);
+    for(let iterator = 0; iterator < array.length; iterator++){
+        array[iterator] = new Array(rows);
+    }
+    return array;
 }
 
 function setup(){
-    grid = make2DArray(10, 10);
+    grid = make2DArray(columns, rows);
+    for(let i = 0; i < columns; i++){
+        for(j = 0; j < rows; j++){
+            grid[i][j] = floor(random(2));
+        }
+    }
 }
 function draw(){
 
